7b5f259a38c63ca199f229c9041c0c6ff365cb86
Patrick Lei <patrick.lei.99@gmail.com>
2025-09-15 21:05:09 -0400

    make everything mine

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -6,16 +6,10 @@ const GlobeComponent = () => {
   let mapContainer: HTMLDivElement | undefined;
 
   const visitedCountries = [
-    "France",
+    "USA",
+    "Canada",
     "China",
-    "Italy",
-    "Sri Lanka",
-    "Turkey",
-    "Greece",
-    "Malta",
-    "Hungary",
-    "Portugal",
-    "Marocco",
+    "Japan",
   ];
 
   onMount(() => {
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,9 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
-  email: "contact@gianmarcocavallo.com<",
+  github: "https://github.com/plei99",
+  arxiv: "https://arxiv.org/a/0000-0003-1977-1596.html",
+  notes: "https://plei99.github.io/notes/",
+  email: "patrick.lei@bc.edu<",
+  cv: "https://plei99.github.io/cv/",
 };
 
 export const loaderAnimation = [
