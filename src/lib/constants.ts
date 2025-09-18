9173c2a6cdfb33b0ff8e97be41be0218aae2adae
Patrick Lei <patrick.lei.99@gmail.com>
2025-09-17 23:11:39 -0400

    update notes link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const LINKS = {
   github: "https://github.com/plei99",
   arxiv: "https://arxiv.org/a/0000-0003-1977-1596.html",
-  notes: "https://plei99.github.io/notes/",
+  notes: "https://plei99.github.io/livetex/",
   email: "patrick.lei@bc.edu<",
   cv: "https://github.com/plei99/cv/releases/latest/download/Patrick_Lei_CV.pdf",
   seminar: "https://sites.google.com/bc.edu/bcags-fall2025/home",
