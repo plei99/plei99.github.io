2d5357296383c984bc671939166567bb9052dff7
Patrick Lei <patrick.lei.99@gmail.com>
2025-09-23 14:38:56 -0400

    bilibili

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,12 @@
 export const LINKS = {
   github: "https://github.com/plei99",
   arxiv: "https://arxiv.org/a/0000-0003-1977-1596.html",
+  ag: "https://arxiv.org/list/math.AG/recent",
   notes: "https://plei99.github.io/livetex/",
   email: "patrick.lei@bc.edu<",
   cv: "https://github.com/plei99/cv/releases/latest/download/Patrick_Lei_CV.pdf",
   seminar: "https://sites.google.com/bc.edu/bcags-fall2025/home",
-  code: "https://github.com/plei99/plei99.github.io",
+  bilibili: "https://space.bilibili.com/3546599313640115?spm_id_from=333.1007.0.0",
 };
 
 export const loaderAnimation = [
